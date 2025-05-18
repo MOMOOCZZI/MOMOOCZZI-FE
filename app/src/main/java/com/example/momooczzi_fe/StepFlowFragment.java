823a53c2ce10911ac60abe0324a1c93bee7af7dd
김지원 <jiwonkim0531@gachon.ac.kr>
2025-05-18 15:47:55 +0900

    feat: 메뉴 추천 step page adapter java 파일 구현

--- app/src/main/java/com/example/momooczzi_fe/StepFlowFragment.java
+++ app/src/main/java/com/example/momooczzi_fe/StepFlowFragment.java
@@ -1,4 +1,31 @@
 package com.example.momooczzi_fe;
 
-public class StepFlowFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class StepFlowFragment extends Fragment {
+
+    private ViewPager2 viewPager;
+
+    public StepFlowFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_step_flow, container, false);
+        viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new StepPagerAdapter(requireActivity()));
+        return view;
+    }
+
+    public void goToStep(int index) {
+        viewPager.setCurrentItem(index, true);
+    }
 }
