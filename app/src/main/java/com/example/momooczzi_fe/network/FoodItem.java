f82dc71805ff7b08b840cd3b13a5628bdb9c51bd
김지원 <jiwonkim0531@gachon.ac.kr>
2025-05-30 15:40:45 +0900

    feat: 음식 추천 리스트 받아오는 Java 파일 구현

--- app/src/main/java/com/example/momooczzi_fe/network/FoodItem.java
+++ app/src/main/java/com/example/momooczzi_fe/network/FoodItem.java
@@ -1,17 +1,25 @@
-package com.example.momooczzi_fe;
+package com.example.momooczzi_fe.network;
 
 public class FoodItem {
     private String food;
     private String description;
     private String image_url;
 
-    public String getFood(){
+    public FoodItem(String food, String description, String image_url) {
+        this.food = food;
+        this.description = description;
+        this.image_url = image_url;
+    }
+
+    public String getFood() {
         return food;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return description;
     }
-    public String getImage_url(){
+
+    public String getImage_url() {
         return image_url;
     }
 }
