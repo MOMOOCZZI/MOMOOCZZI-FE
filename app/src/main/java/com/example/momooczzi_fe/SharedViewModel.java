b8ce9b559820496def9b1a4d8e4c308414abbcc5
김지원 <jiwonkim0531@gachon.ac.kr>
2025-05-30 15:43:01 +0900

    feat: 음식 추천 관련 타입 변경

--- app/src/main/java/com/example/momooczzi_fe/SharedViewModel.java
+++ app/src/main/java/com/example/momooczzi_fe/SharedViewModel.java
@@ -3,39 +3,21 @@ package com.example.momooczzi_fe;
 import androidx.lifecycle.ViewModel;
 
 public class SharedViewModel extends ViewModel {
-    private boolean gender;
+    private Boolean gender;
     private String emotion;
-    private String weather;
-    private String heapen;
+    private String happen;
+    private Double latitude;
+    private Double longitude;
 
+    public void setGender(Boolean g) { this.gender = g; }
+    public void setEmotion(String e) { this.emotion = e; }
+    public void setHappen(String h) { this.happen = h; }
+    public void setLatitude(Double lat) { this.latitude = lat; }
+    public void setLongitude(Double lon) { this.longitude = lon; }
 
-    public boolean getGender() {
-        return gender;
-    }
-
-    public void setGender(boolean gender) {
-        this.gender = gender;
-    }
-
-    public String getEmotion() {
-        return emotion;
-    }
-
-    public void setEmotion(String emotion) {
-        this.emotion = emotion;
-    }
-
-    public String getWeather() {
-        return weather;
-    }
-
-    public void setWeather(String weather) {
-        this.weather = weather;
-    }
-    public String getHeapen(){
-        return heapen;
-    }
-    public void setHeapen(String heapen){
-        this.heapen = heapen;
-    }
+    public Boolean getGender() { return gender; }
+    public String getEmotion() { return emotion; }
+    public String getHappen() { return happen; }
+    public Double getLatitude() { return latitude; }
+    public Double getLongitude() { return longitude; }
 }
